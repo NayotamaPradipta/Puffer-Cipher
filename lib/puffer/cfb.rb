6e1d38ecc6b1ce2991d061d899def577820af8cd
Muhammad Rakha Athaya <rakhasennin@gmail.com>
2024-03-23 11:35:38 +0700

    feat: add cfb mode decryption

--- lib/puffer/cfb.rb
+++ lib/puffer/cfb.rb
@@ -21,8 +21,23 @@ module Puffer
             encrypted_blocks.join
         end
 
-        def decrypt(text)
-            "Decrypted #{text} with key #{@key} in CFB mode"
+        def decrypt(base64_text)
+            r_size = 1 # 8-bit / 1-byte CFB by default
+            PufferFunction.initialize_p_array(@key)
+            PufferFunction.initialize_s_box(@key)
+
+            binary_data = base64_to_binary(base64_text)
+            iv, ciphertext = binary_data[0...128], binary_data[128..-1]
+            shift_register = iv
+
+            decrypted_blocks = ciphertext.chars.each_slice(r_size*8).map(&:join).map do |block|
+                block_decryptor = PufferFunction.f_function_encrypt(shift_register, key)[0...(r_size*8)]
+                decrypted_block = xor_blocks(block_decryptor, block)
+                shift_register = shift(shift_register,block,r_size)
+                decrypted_block
+            end
+
+            decrypted_blocks.join
         end
 
         def random_iv(length=128)
