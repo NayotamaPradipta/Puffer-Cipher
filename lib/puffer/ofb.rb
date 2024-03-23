a4a3c72aa3b8097232fc7ca72a5257a597e66350
Muhammad Rakha Athaya <rakhasennin@gmail.com>
2024-03-23 11:47:00 +0700

    feat: add ofb mode decryption

--- lib/puffer/ofb.rb
+++ lib/puffer/ofb.rb
@@ -19,8 +19,22 @@ module Puffer
             encrypted_blocks.join
         end
 
-        def decrypt(text)
-            "Decrypted #{text} with key #{@key} in OFB mode"
+        def decrypt(base64_text)
+            r_size = 1 # 8-bit / 1-byte OFB by default
+            PufferFunction.initialize_p_array(@key)
+            PufferFunction.initialize_s_box(@key)
+
+            binary_data = base64_to_binary(base64_text)
+            shift_register = "0"*128 # Shift register innit with zeroes
+
+            decrypted_blocks = binary_data.chars.each_slice(r_size*8).map(&:join).map do |block|
+                block_decryptor = PufferFunction.f_function_encrypt(shift_register, key)[0...(r_size*8)]
+                decrypted_block = xor_blocks(block_decryptor, block)
+                shift_register = shift(shift_register,block_decryptor,r_size)
+                decrypted_block
+            end
+
+            decrypted_blocks.join
         end
 
         def xor_blocks(block1, block2)
