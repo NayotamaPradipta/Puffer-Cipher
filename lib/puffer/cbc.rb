f23f5531cc46a9fbef9645b590aa42d5b2192145
Muhammad Rakha Athaya <rakhasennin@gmail.com>
2024-03-23 11:27:46 +0700

    feat: add cbc mode decryption

--- lib/puffer/cbc.rb
+++ lib/puffer/cbc.rb
@@ -23,25 +23,36 @@ module Puffer
             encrypted_blocks.join
         end
 
-    def decrypt(base64_text)
-        "Decrypted #{text} with key #{@key} in Counter mode"
-    end
+        def decrypt(base64_text)
+            PufferFunction.initialize_p_array(@key)
+            PufferFunction.initialize_s_box(@key)
 
-    private
+            binary_data = base64_to_binary(base64_text)
+            iv, ciphertext = binary_data[0...128], binary_data[128..-1]
+            previous_block = iv
 
-    def random_iv(length=128)
-        iv = ""
-        length.times { iv << rand(2).to_s }
-        iv
-      end
+            decrypted_blocks = ciphertext.chars.each_slice(BLOCK_SIZE*8).map(&:join).map do |block|
+                decrypted_block = PufferFunction.f_function_decrypt(block, key)
+                plaintext_block = xor_blocks(decrypted_block, previous_block)
+                previous_block = block
+                plaintext_block
+            end
 
-    def xor_blocks(block1, block2)
-        result = ""
-        block1.length.times do |i|
-          result << (block1[i].to_i ^ block2[i].to_i).to_s
+            decrypted_blocks.join
         end
-        result
-    end
 
+        def random_iv(length=128)
+            iv = ""
+            length.times { iv << rand(2).to_s }
+            iv
+        end
+
+        def xor_blocks(block1, block2)
+            result = ""
+            block1.length.times do |i|
+            result << (block1[i].to_i ^ block2[i].to_i).to_s
+            end
+            result
+        end
     end
 end
